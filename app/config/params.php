9510762dd0fb80a728c9f825d5df516340ea3035
MorpheusIV <miltongallarday@gmail.com>
2022-05-05 15:17:33 +0200

    Structure MVC de base

---
+++ app/config/params.php
@@ -0,0 +1,15 @@
+<?php
+/*
+  ./app/config/parametres.php
+  PARAMETRES DE L'APPLICATION
+ */
+
+  // INITIALISATION ZONES DYNAMIQUES
+    $content = '';
+   
+
+  // PARAMETRES DE CONNEXION
+    define('DB_HOST', 'localhost:3306');
+    define('DB_NAME', 'readit_2020');
+    define('DB_USER', 'root');
+    define('DB_PWD', 'root');
